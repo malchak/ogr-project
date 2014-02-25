42f43d4e0f23f2db66546727c29b9023e546da29
Michael Malchak <michaelmalchak@gmail.com>
2014-02-24 22:26:06 -0800

    animate scroll for page navigation

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
 	$('li > a' ).click(function(){
+		var aTagId = $(this).attr("id");
+		var target = $("a[name='"+ aTagId +"']");
+		
 		$('li').removeClass();
 		$(this).parent().addClass('active');
+		$('html,body').animate({scrollTop: target.offset().top},'slow');
 	});
 
 });
+
+// function scrollToAnchor(aid){
+//     var aTag = $("a[name='"+ aid +"']");
+//     $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+// }
+
+// scrollToAnchor('id3');
